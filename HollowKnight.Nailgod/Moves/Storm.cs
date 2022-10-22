d9bc0e0133cea0a7d7daa1cda9ae32896cb8a702
Nearrin Yu <nearrin@outlook.com>
2022-10-22 15:27:41 -0700

    Update Moves/Storm.

--- HollowKnight.Nailgod/Moves/Storm.cs
+++ HollowKnight.Nailgod/Moves/Storm.cs
@@ -6,6 +6,7 @@ public class Storm
         fsm.AddState("Storm/Start");
         fsm.AddState("Storm/Hide");
         fsm.AddState("Storm/Show");
+        fsm.AddState("Storm/Charged");
         fsm.AddState("Storm/Slash");
         fsm.AddCustomAction("Storm/Start", () =>
         {
@@ -18,14 +19,35 @@ public class Storm
             var sly = fsm.gameObject;
             sly.transform.position = Vector3.zero;
         });
-        fsm.AddAction("Storm/Hide", fsm.CreateWait(0.25f, fsm.GetFSMEvent("FINISHED")));
+        fsm.AddAction("Storm/Hide", fsm.CreateWait(0.5f, fsm.GetFSMEvent("FINISHED")));
         fsm.AddTransition("Storm/Hide", "FINISHED", "Storm/Show");
         fsm.AddCustomAction("Storm/Show", () =>
         {
             var sly = fsm.gameObject;
             sly.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
-            sly.transform.position = new Vector3(46, 8, 0.0061f);
+            var xMin = 34f;
+            var xMax = 59f;
+            var yMin = 7f;
+            var yMax = 16f;
             var hero = HeroController.instance.gameObject;
+            xMin = Math.Max(xMin, hero.transform.position.x - 12.5f);
+            xMax = Math.Min(xMax, hero.transform.position.x + 12.5f);
+            float x, y;
+            while (true)
+            {
+                x = UnityEngine.Random.Range(xMin, xMax);
+                y = UnityEngine.Random.Range(yMin, yMax);
+                if (Math.Abs(x - hero.transform.position.x) < 4)
+                {
+                    continue;
+                }
+                if (Math.Abs(y - hero.transform.position.y) < 4)
+                {
+                    continue;
+                }
+                break;
+            }
+            sly.transform.position = new Vector3(x, y, 0.0061f);
             if (sly.transform.position.x < hero.transform.position.x)
             {
                 sly.transform.localScale = new Vector3(-1, 1, 1);
@@ -34,16 +56,32 @@ public class Storm
             {
                 sly.transform.localScale = new Vector3(1, 1, 1);
             }
-            sly.transform.Find("NA Charged").gameObject.SetActive(true);
+            sly.transform.Find("NA Charge").gameObject.SetActive(true);
+            sly.transform.Find("NA Charged").gameObject.SetActive(false);
         });
         fsm.AddAction("Storm/Show", fsm.CreateGeneralAction(() =>
          {
              var hero = HeroController.instance.gameObject;
              var sly = fsm.gameObject;
              var v = hero.transform.position - sly.transform.position;
-             sly.GetComponent<Rigidbody2D>().velocity = v;
+             sly.GetComponent<Rigidbody2D>().velocity = v * 0.125f;
          }));
-        fsm.AddAction("Storm/Show", fsm.CreateWait(0.125f, fsm.GetFSMEvent("FINISHED")));
-        fsm.AddTransition("Storm/Show", "FINISHED", "Storm/Hide");
+        fsm.AddAction("Storm/Show", fsm.CreateWait(0.1f, fsm.GetFSMEvent("FINISHED")));
+        fsm.AddTransition("Storm/Show", "FINISHED", "Storm/Charged");
+        fsm.AddCustomAction("Storm/Charged", () =>
+        {
+            var sly = fsm.gameObject;
+            sly.transform.Find("NA Charge").gameObject.SetActive(false);
+            sly.transform.Find("NA Charged").gameObject.SetActive(true);
+        });
+        fsm.AddAction("Storm/Charged", fsm.CreateGeneralAction(() =>
+        {
+            var hero = HeroController.instance.gameObject;
+            var sly = fsm.gameObject;
+            var v = hero.transform.position - sly.transform.position;
+            sly.GetComponent<Rigidbody2D>().velocity = v * 0.125f;
+        }));
+        fsm.AddAction("Storm/Charged", fsm.CreateWait(0.1f, fsm.GetFSMEvent("FINISHED")));
+        fsm.AddTransition("Storm/Charged", "FINISHED", "Storm/Hide");
     }
 }
